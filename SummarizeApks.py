39f07a8c83b053625711daa28fcd6f9ff3f85eec
Cesar Salazar <cesarl_88@hotmail.com>
2018-10-08 13:07:12 -0400

    summarize apks and report how many are good and how many have been packed

--- SummarizeApks.py
+++ SummarizeApks.py
@@ -1,10 +1,14 @@
 import os
+import subprocess, sys
+import json
 
 if __name__ == '__main__':
 
 	Categories = [dI for dI in os.listdir('.') if os.path.isdir(os.path.join('.',dI))]#filter(lambda x: os.path.isdir(x), os.listdir('.'))
 	if not os.path.exists('./_ValidApps/'):
 		os.mkdir('./_ValidApps/')
+
+	Report = ""
 
 	for cat in Categories:
 		if cat != ".git":
@@ -15,6 +19,7 @@ if __name__ == '__main__':
 			if not os.path.exists(Valid_apks_path):
 				os.mkdir(Valid_apks_path)
 
+			Report += cat + "\n"
 			NumberOfApps = 0
 			for app in cat_apps:
 				apk_path = os.path.join(folder_app,app)
@@ -28,11 +33,38 @@ if __name__ == '__main__':
 
 				if len(apks) >= 12:
 
+					apks_num = 0
 					for apk in apks:
 						print("about to run apkID")
-						os.system("apkid " + apk)
+						cmd = "apkid --json " + apk 
+						print(cmd)
+						p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+						(output, err) = p.communicate()
+ 
+						## Wait for date to terminate. Get return returncode ##
+						p_status = p.wait()
+						if output:
+							out = json.loads(str(output))
+							for x in out:
+								xx= out[x]
+								for y in xx:
+									if y == 'packer':
+										Report+= apk + "\n"
+										apks_num += 1
 
-					NumberOfApps+= 1
+					Report+= app + "\n"
+					Report+= "Number of APKS("+str(len(apks))+")" + "\n"
+					Report+= "Number of APKS Good to go("+str(len(apks) - apks_num)+")" + "\n"
+					Report+= "Number of APKS Packed("+str(apks_num)+")\n\n"
+
+					print(Report)
+					f = open("./_ValidApps/" + cat + "/" + cat + "(Report).txt","w+")
+					f.write(Report)
+					f.close()
+
+					if len(apks) - apks_num > 12:
+						NumberOfApps+= 1
+						
 					app = app.replace(" ", "\ ")
 					newpath = "./_ValidApps/" + cat + "/" + app + "/"
 					Command = "mkdir " + newpath
@@ -41,6 +73,7 @@ if __name__ == '__main__':
 					Command = "cp -r ./" + cat + "/" + app +" ./_ValidApps/" + cat + "/"
 					print("Copying to " + newpath)
 					os.system(Command)
+
 
 			print("There are " + str(NumberOfApps) + " Valid apps for this category")
 			f = open("./_ValidApps/" + cat + "/" + cat + "(" + str(NumberOfApps) + ").txt","w+")
